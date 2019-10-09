4860d5cf3e6fb726952524de17d02427a597b9ca
Nick Lunn <nrlunn325@gmail.com>
2019-10-09 11:37:27 -0400

    kooky

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+
+
 const form = document.querySelector('#create-task-form')
+
+// const butt = document.createElement('select')
+// butt.id = 'priority'
+// butt.label = 'Priority'
+// let options = ['high', 'medium', 'low']
+// for (let i = 0; i < options.length; i++){
+//   let option = document.createElement('option')
+//   option.value = options[i]
+//   option.innerText = options[i]
+//   butt.appendChild(option)
+// }
+
+// form.appendChild(butt)
+
 // 1. create button
 
 const button = document.createElement('button')
@@ -14,13 +30,18 @@ button.innerText = 'x'
 
 // 2. slap it on the dom
 
+// 1. select input 
+const priority = document.querySelector('#priority')
+
 form.addEventListener("submit", function(e) {
   e.preventDefault();
+  debugger;
   const inputField = document.querySelector("#new-task-description");
   let input = inputField.value;
   const taskList = document.querySelector("#tasks");
   let listItem = document.createElement("li");
   listItem.innerText = input;
+  listItem.textColor = priority.value
   taskList.appendChild(listItem);
   listItem.appendChild(button);
   form.reset();
