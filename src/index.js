dafb2b402d0af67e5fe3a0331d99c35a40e122af
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-09 10:38:00 -0400

    deliverables without stretch goals

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
 });
+
+const form = document.querySelector('#create-task-form')
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const inputField = document.querySelector("#new-task-description");
+  let input = inputField.value;
+  const taskList = document.querySelector("#tasks");
+  let listItem = document.createElement("li");
+  listItem.innerText = input;
+  taskList.appendChild(listItem);
+  form.reset();
+})
+
+
+
+
+
